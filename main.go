b5980d84b7b347109d5bfd3a22c3f0366e2f2544
alqassmiii <qassimhassan9@gmail.com>
2024-11-21 21:33:02 +0300

    20 % of front end

--- main.go
+++ main.go
@@ -3,19 +3,18 @@ package main
 import (
 	"fmt"
 	"forum/src/server"
-
 	"log"
 	"net/http"
 )
 
 
 func main() {
-	 http.NewServeMux()
-
-    http.Handle("/Static/", http.StripPrefix("/Static/", http.FileServer(http.Dir("Static"))))
+   http.NewServeMux()
+   http.Handle("/templates/", http.StripPrefix("/templates/", http.FileServer(http.Dir("templates"))))
+   http.Handle("/Static/", http.StripPrefix("/Static/", http.FileServer(http.Dir("Static"))))
    http.HandleFunc("/", server.MainPage)
 
 	
-	fmt.Println("Server is listening on port 8080")
-	log.Fatal(http.ListenAndServe(":8080", nil))
-}
+   fmt.Println("Server is listening on port 8080")
+   log.Fatal(http.ListenAndServe(":8080", nil))
+}   
--- src/server/server.go
+++ src/server/server.go
@@ -11,17 +11,19 @@ func MainPage(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path != "/" {
 		// error 404
 		ErrorHandler(w, r, http.StatusNotFound, "")
-	
+		http.ServeFile(w, r, "templates/error.html")
 		return
 	}
-	tmpl, err := template.ParseFiles("Templates/index.html")
+	tmpl, err := template.ParseFiles("templates/index.html")
 	if err != nil {
 		// error 500
 		ErrorHandler(w, r, http.StatusInternalServerError, "")
+		http.ServeFile(w, r, "templates/error.html")
 		return
 	}
 	tmpl.Execute(w, r)
 }
+
 
 func ErrorHandler(w http.ResponseWriter, r *http.Request, statusCode int, errM string) {
 	var errorMessage string
@@ -29,9 +31,11 @@ func ErrorHandler(w http.ResponseWriter, r *http.Request, statusCode int, errM s
 	case http.StatusNotFound:
 		//404
 		errorMessage = " 404 - Page not found"
+		http.ServeFile(w, r, "templates/error.html")
 	case http.StatusBadRequest:
 		//400
 		errorMessage = "400 - Bad request"
+		http.ServeFile(w, r, "templates/error.html")
 		if errM != "" {
 			//400 with extra message
 			errorMessage += ": " + errM
@@ -39,11 +43,14 @@ func ErrorHandler(w http.ResponseWriter, r *http.Request, statusCode int, errM s
 	case http.StatusInternalServerError:
 		//500
 		errorMessage = "500 - Internal server error"
+		http.ServeFile(w, r, "templates/error.html")
 	case http.StatusMethodNotAllowed:
 		//405
 		errorMessage = "405 - Method not allowed"
+		http.ServeFile(w, r, "templates/error.html")
 	default:
 		errorMessage = "Unexpected error"
+		http.ServeFile(w, r, "templates/error.html")
 	}
 	w.WriteHeader(statusCode)
 	fmt.Fprintf(w, errorMessage)
