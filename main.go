17dc77aaf6d3e6c7433cb282fd85ca4f20c333b9
Sayed Ahmed Husain <160347327+sahmedhusain@users.noreply.github.com>
2024-11-26 21:04:57 +0300

    dockerfile created

--- main.go
+++ main.go
@@ -10,7 +10,7 @@ import (
 func main() {
 	http.NewServeMux()
 	http.Handle("/templates/", http.StripPrefix("/templates/", http.FileServer(http.Dir("templates"))))
-	http.Handle("/Static/", http.StripPrefix("/Static/", http.FileServer(http.Dir("Static"))))
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
 	http.HandleFunc("/", server.MainPage)
 
 	fmt.Println("Server running on http://localhost:8080 \nTo stop the server press Ctrl+C")
