d12d276694206acb942020817076758ad873d712
Sayed Ahmed Husain <160347327+sahmedhusain@users.noreply.github.com>
2024-12-19 20:13:30 +0300

    format

--- main.go
+++ main.go
@@ -1,33 +1,33 @@
 package main
 
 import (
-	"fmt"
-	db "forum/database"
-	"forum/src/server"
-	"log"
-	"net/http"
+    "fmt"
+    db "forum/database"
+    "forum/src/server"
+    "log"
+    "net/http"
 )
 
 func init() {
-	db.DataBase()
+    db.DataBase()
 }
 
 func main() {
-	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("./static/"))))
+    http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("./static/"))))
 
-	http.HandleFunc("/", server.MainPage)
-	http.HandleFunc("/login", server.LoginPage)
-	http.HandleFunc("/signup", server.SignupPage)
-	http.HandleFunc("/home", server.HomePage)
-	http.HandleFunc("/newpost", server.NewPostPage)
-	http.HandleFunc("/settings", server.SettingsPage)
+    http.HandleFunc("/", server.MainPage)
+    http.HandleFunc("/login", server.LoginPage)
+    http.HandleFunc("/signup", server.SignupPage)
+    http.HandleFunc("/home", server.HomePage)
+    http.HandleFunc("/newpost", server.NewPostPage)
+    http.HandleFunc("/settings", server.SettingsPage)
     http.HandleFunc("/notifications", server.NotificationsPage)
-	http.HandleFunc("/myprofile", server.MyProfilePage)
-	http.HandleFunc("/profile", server.ProfilePage)
-	http.HandleFunc("/admin", server.AdminPage)
-	http.HandleFunc("/moderator", server.ModeratorPage)
+    http.HandleFunc("/myprofile", server.MyProfilePage)
+    http.HandleFunc("/profile", server.ProfilePage)
+    http.HandleFunc("/admin", server.AdminPage)
+    http.HandleFunc("/moderator", server.ModeratorPage)
 
-	fmt.Println("Server running on http://localhost:8080\nTo stop the server press Ctrl+C")
+    fmt.Println("Server running on http://localhost:8080\nTo stop the server press Ctrl+C")
 
-	log.Fatal(http.ListenAndServe(":8080", nil))
+    log.Fatal(http.ListenAndServe(":8080", nil))
 }
