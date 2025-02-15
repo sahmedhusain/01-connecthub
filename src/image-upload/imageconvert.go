ee888289e055cbef746a98eb1d4639a7cdb1dd8c
Sayed Ahmed Husain <160347327+sahmedhusain@users.noreply.github.com>
2025-02-15 06:57:15 +0300

    commit test

--- src/image-upload/imageconvert.go
+++ src/image-upload/imageconvert.go
@@ -2,6 +2,6 @@ package image
 
 import "encoding/base64"
 
-func base64EncodeImage(img []byte) string {
+func Base64EncodeImage(img []byte) string {
 	return base64.StdEncoding.EncodeToString(img)
 }
